88147b8cb60936cea1689f9cdd0c7ac4aa7a9e1d
Gbemi <preciousbolarinwa2026@gmail.com>
2024-09-04 21:06:14 -0500

    Set up plain three.js

--- cellscape/src/App.jsx
+++ cellscape/src/App.jsx
@@ -1,12 +1,18 @@
 import React from "react"
-import Navbar from "./pages/homepage/navbar"
+import Navbar from "./pages/static/navbar" 
+import './App.css';
+import { Canvas } from "@react-three/fiber"
 
 function App() {
+  <>
   return (
-    <div>
-      <Navbar />
-    </div>
+    <Canvas>
+      <Main />
+
+    </Canvas>
+   
   )
+  </>
 }
 
 export default App
