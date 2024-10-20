437d2943467873190371bd85714c9df8314d79d1
Gbemi <preciousbolarinwa2026@gmail.com>
2024-10-20 00:28:39 -0500

    Create front page animation on user arrival

--- cellscape/src/App.jsx
+++ cellscape/src/App.jsx
@@ -6,34 +6,35 @@ import { Routes, Route, BrowserRouter} from "react-router-dom"
 import About from './pages/static/about/about';
 import Contact from './pages/static/contact/contact'
 import Navbar from './pages/static/navbar/navbar'
-
 import Home from './pages/home';
 import '@mantine/core/styles.css';
 
+import { Canvas } from '@react-three/fiber';
+import { Environment, OrbitControls } from '@react-three/drei';
+import Bloodcell from "../public/Bloodcell.jsx"
 
-const App = () => {
-  const [count, setCount ] = useState(0)
+
+function App() {
+  const [count, setCount] = useState(0)
+
   return (
-    <div className = "container">
-     
-        <Navbar />
+    <div>
+      <Navbar />
 
         
-        <Routes>
-          <Route exact path="/" element={<Home />} />
-          <Route  path="/about" element={<About />} />
-          <Route  path="/contact" element={<Contact />} />
-        </Routes>
+    <Routes>
+      <Route exact path="/" element={<Home />} />
+      <Route  path="/about" element={<About />} />
+      <Route  path="/contact" element={<Contact />} />
+   </Routes>
 
-  
 
-      
- 
     </div>
-  )}
-
-
-
-
+   
+  )
+}
 
 export default App
+
+
+/* */
--- cellscape/src/pages/home.jsx
+++ cellscape/src/pages/home.jsx
@@ -1,13 +1,73 @@
+
 import React from "react";
+import "../home.css"
+import { useLayoutEffect, useRef } from "react";
+import gsap from "gsap"
+import Text from "./text";
+
+
+
 
 const Home = () =>{
-    <>
+    const comp = useRef(null)
+
+    useLayoutEffect(() =>{
+        let ctx = gsap.context(() => {
+            const t1 = gsap.timeline()
+            t1.from("#intro-slider",{
+                xPercent:"-100",
+                duration:1.3,
+                delay:0.3,
+
+            }).from(["#title-1", "#title-2", "#title-3"],{
+                opacity:0,
+                y:"+=30",
+                stagger:0.5,
+
+            }).to(["#title-1", "#title-2", "#title-3"],{
+                opacity:0,
+                y:"-=30",
+                delay:0.3,
+                stagger:0.5,
+            }).to("intro-slider",{
+                xPercent:"-100",
+                duration:1.3,
+
+            })
+           
+
+
+        }, comp)
+        return () => ctx.revert()
+
+    },[])
+
     return (
-        <img src="src\assets\logo2.png"  alt="CellScape Logo" className='logo'/>
-        <h1>Home</h1>
+        <div className="top full-screen" ref={comp}>
+            <div className="animated" id="intro-slider">
+                <h1 id="title-1">Welcome</h1>
+                <h1 id="title-2">To</h1>
+                <h1 id="title-3">Cellscape</h1>
+            </div>
+              <div className="main">
+                 <h1 className="main-text" id="welcome">Cellscape</h1>
+             </div>  
+
+
+             
+        </div>
+
+        
+      
+        
+
+
 
     )
-    </>
+        
+
+    
+    
 }
 
 export default Home 
