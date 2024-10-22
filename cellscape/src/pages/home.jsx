9dad0592187c2c8721165795424eba963ddd6965
Gbemi <preciousbolarinwa2026@gmail.com>
2024-10-21 22:45:06 -0500

    Set up text of home page and fix font glitch

--- cellscape/src/pages/home.jsx
+++ cellscape/src/pages/home.jsx
@@ -1,5 +1,5 @@
 import React, { useState, useLayoutEffect, useRef } from "react";
-import "../home.css";
+import "../pages/home.css";
 import gsap from "gsap";
 
 const Home = () => {
@@ -42,7 +42,13 @@ const Home = () => {
             {/* Animation Section */}
             <div className="full-screen animated" id="intro-slider">
                 <h1 id="title-1">Welcome</h1>
+                <h1> </h1>
+                <h1> </h1>
+                <h1> </h1>
                 <h1 id="title-2">To</h1>
+                <h1> </h1>
+                <h1> </h1>
+                <h1> </h1>
                 <h1 id="title-3">Cellscape</h1>
             </div>
 
@@ -51,81 +57,79 @@ const Home = () => {
 
             {/* Content Section */}
             <div className="content">
-            <section>
-                    <h1 className="reveal-type">
+
+                <section>
+                    <div className="section-1">
+                <h1 className="reveal-type header"  >
                          Red Blood Cell
                     </h1>
-                </section>
-                <section>
+                    <div className="text-image">
                     <p className="reveal-type">
                     This is a red blood cell, also known as an Erythrocyte. They transport nutrients and gas throughout the body. They live to around 120 days. The shape of a blood cell allows it to be able to navigate the cardiovascular system easily.
                     </p>
+                    <img  className="blood-cell" src="src/assets/bloodcell.png"/>
+                    </div>
+                    </div>
                 </section>
 
+               
                 <section>
-                    <h1 className="reveal-type">
-                         ??
+                <h1 className="reveal-type header">
+                         Sickle Cell
                     </h1>
-                </section>
-                <section>
                     <p className="reveal-type">
                     This is also a red blood cell. It also transports nutrients and gas throughout the body. However, here's the difference, it only lives to 60 days and has a quite abnormal shape, doesn't it?
                     </p>
                 </section>
                 
+             
                 <section>
-                    <h1 className="reveal-type">
+                <h1 className="reveal-type header">
                          Genetics
                     </h1>
-                </section>
-                <section>
                     <p className="reveal-type">
                     This is caused by this change in DNA, specifically in the HBB, aka the hemoglobin gene. Glu which is what allows a normal blood cell to form is now val instead.
                     </p>
                 </section>
 
+              
                 <section>
-                    <h1 className="reveal-type">
+                <h1 className="reveal-type header">
                          Inheritance
                     </h1>
-                </section>
-                <section>
                     <p className="reveal-type">
                     People with sickle cell anemia inherit hemoglobin gene trait S from both parents. Those that only inherit from one parent have sickle cell trait, and can pass sickle cell anemia on if their partner has at least one S trait.
                     </p>
                 </section>
 
 
+                
                 <section>
-                    <h1 className="reveal-type">
+                <h1 className="reveal-type header">
                          What's bad about it?
                     </h1>
-                </section>
-                <section>
                     <p className="reveal-type">
                     Due to oxygen being cut off due to blockage caused by the sickle cells, the person feels immense pain and is unable to move for hours, days, sometimes weeks and even months. If you think you can imagine the pain, you can not. Oxygen can be cut off anywhere in the body: legs, brain, heart, lungs, and more. This could lead to death.
                     </p>
                 </section>
 
 
+               
                 <section>
-                    <h1 className="reveal-type">
+                <h1 className="reveal-type header">
                          Treatments
                     </h1>
-                </section>
-                <section>
                     <p className="reveal-type">
                     DHydration, hydroxreau, bone marrow treatment, and a recently developed treatment CRISPR. 
                     </p>
                 </section>
 
             
+                
                 <section>
-                    <h1 className="reveal-type">
+                <h1 className="reveal-type header">
                          Completed
                     </h1>
-                </section>
-                <section>
                     <p className="reveal-type">
                     This was a basic overview into sickle cell. If you would like to gain more info about sickle cell and how it works biology-wise, here are some great links.
                     </p>
--- cellscape/src/pages/static/about/about.jsx
+++ cellscape/src/pages/static/about/about.jsx
@@ -10,7 +10,7 @@ const About = () => {
       <div className='tags'>
       <h1>About Me</h1>
       <h2> Gbemisola Bolarinwa - Developer</h2>
-      <p> Hi, I'm Gbemi, a teenage web developer. This is my first ever website that I've created and published.This website is meant to spread awareness for Sickle Cell Anemia. This is just the first version of the site and I'll continously improve it in order for people to gain more awareness about it. Thank you! </p>
+      <p className='paragraph'> Hi, I'm Gbemi, a teenage web developer. This is my first ever website that I've created and published.This website is meant to spread awareness for Sickle Cell Anemia. This is just the first version of the site and I'll continously improve it in order for people to gain more awareness about it. Thank you! </p>
       </div>
 
   
